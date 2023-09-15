206d0e40c4b55eff318513158bf3f6a9f098acd5
larrysfinolb <larrysfinolb@gmail.com>
2023-09-15 11:05:02 -0400

    feat: description to dishes

--- src/schemas/categorySchema.js
+++ src/schemas/categorySchema.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const id = Joi.string().regex(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/);
 const name = Joi.string().regex(/^[\w\s\-_,.()À-ÿ]{1,50}$/);
-const description = Joi.string().regex(/^[\w\s\-_,.()À-ÿ]{1,50}$/);
+const description = Joi.string().regex(/^[\w\s\-_,.()À-ÿ]{1,255}$/);
 
 const schemaParams = Joi.object({
   categoryId: id.required(),
--- src/schemas/dishSchema.js
+++ src/schemas/dishSchema.js
@@ -2,6 +2,7 @@ import Joi from 'joi';
 
 const id = Joi.string().regex(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/);
 const name = Joi.string().regex(/^[\w\s\-_,.()À-ÿ]{1,50}$/);
+const description = Joi.string().regex(/^[\w\s\-_,.()À-ÿ]{1,255}$/);
 const price = Joi.number().min(0).max(9999.99);
 
 const schemaParams = Joi.object({
@@ -11,12 +12,14 @@ const schemaParams = Joi.object({
 const schemaBodyCreate = Joi.object({
   categoryId: id,
   name: name.required(),
+  description,
   price: price.required(),
 });
 
 const schemaBodyUpdate = Joi.object({
   categoryId: id,
   name,
+  description,
   price,
 });
 
