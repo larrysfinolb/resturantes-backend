30312bb43a76fc366911af54cba9d8c154de1a79
Larry Finol <larrysfinolb@gmail.com>
2023-11-09 12:20:57 -0400

    feat: add filter to select only delivered  orders and update queries for frequentTables and popularDishes

--- src/services/admin/statisticService.js
+++ src/services/admin/statisticService.js
@@ -8,7 +8,7 @@ const getAll = async ({ year }) => {
 
     year = year ?? new Date().getFullYear();
 
-    const query1 = `SELECT * FROM orders`;
+    const query1 = `SELECT * FROM orders WHERE status = 'delivered'`;
     const { rows: rows1 } = await client.query(query1);
     const result1 = rows1;
 
@@ -31,59 +31,70 @@ const getAll = async ({ year }) => {
       labels: MONTHS,
       data: MONTHS.map(
         (_, i) =>
-          result1.filter(
-            (order) =>
-              order.createdAt.getMonth() === i && order.createdAt.getFullYear() === year && order.status === 'delivered'
-          ).length
+          result1.filter((order) => order.createdAt.getMonth() === i && order.createdAt.getFullYear() === year).length
       ),
     };
     const totalOrders = {
       labels: MONTHS,
       data: MONTHS.map((_, i) =>
         result1
-          .filter(
-            (order) =>
-              order.createdAt.getMonth() === i && order.createdAt.getFullYear() === year && order.status === 'delivered'
-          )
+          .filter((order) => order.createdAt.getMonth() === i && order.createdAt.getFullYear() === year)
           .reduce((acc, curr) => acc + curr.total, 0)
       ),
     };
 
-    const query2 = `SELECT * FROM tables`;
+    const query2 = `SELECT tables.id, tables.description, orders."createdAt"
+    FROM orders 
+    JOIN tables ON orders."tableId" = tables.id
+    WHERE orders.status = 'delivered'`;
     const { rows: result2 } = await client.query(query2);
 
+    // Contar la cantidad de veces que se repite cada mesa y convertimos el objeto en un array
+    const tablesArray = Object.values(
+      result2.reduce((acc, curr) => {
+        if (acc[curr.id]) {
+          acc[curr.id].quantity += 1;
+        } else {
+          acc[curr.id] = { ...curr, quantity: 1 };
+        }
+        return acc;
+      }, {})
+    );
+    // Ordenamos el array por las mesas más frecuentes
+    tablesArray.sort((a, b) => b.quantity - a.quantity);
+    // Convertimos el array en un objeto con las propiedades labels y data
     const frequentTables = {
-      labels: result2.map((table) => table.description),
-      data: result2.map(
-        (table) =>
-          result1.filter(
-            (order) =>
-              order.tableId === table.id && order.createdAt.getFullYear() === year && order.status === 'delivered'
-          ).length
-      ),
+      labels: tablesArray.filter((table) => table.createdAt.getFullYear() === year).map((table) => table.description),
+      data: tablesArray.filter((table) => table.createdAt.getFullYear() === year).map((table) => table.quantity),
     };
 
-    const query3 = `SELECT dishes_orders.*, dishes.name AS "dishName" FROM dishes_orders 
-    JOIN dishes ON dishes_orders."dishId" = dishes.id`;
+    const query3 = `SELECT dishes_orders."orderId", dishes_orders.quantity, dishes.name AS "dishName", dishes.id AS "dishId", orders."createdAt"
+    FROM dishes_orders 
+    JOIN orders ON dishes_orders."orderId" = orders.id
+    JOIN dishes ON dishes_orders."dishId" = dishes.id
+    WHERE orders.status = 'delivered'`;
     const { rows: result3 } = await client.query(query3);
 
-    const popularDishes = {
-      labels: result3.map((dish) => dish.dishName),
-      data: result3.map(
-        (dish) =>
-          result1.filter(
-            (order) =>
-              order.id === dish.orderId && order.createdAt.getFullYear() === year && order.status === 'delivered'
-          ).length * dish.quantity
-      ),
-    };
-    popularDishes.data.sort((a, b) => b - a);
-    popularDishes.labels.sort(
-      (a, b) =>
-        popularDishes.data[popularDishes.labels.indexOf(b)] - popularDishes.data[popularDishes.labels.indexOf(a)]
+    // Los platos que se repiten acumularán la cantidad de veces que se repiten y convertimos el objeto en un array
+    const dishesArray = Object.values(
+      result3.reduce((acc, curr) => {
+        if (acc[curr.dishId]) {
+          acc[curr.dishId].quantity += curr.quantity;
+        } else {
+          acc[curr.dishId] = curr;
+        }
+        return acc;
+      }, {})
     );
-    popularDishes.data = popularDishes.data.slice(0, 5);
-    popularDishes.labels = popularDishes.labels.slice(0, 5);
+    // Ordenamos el array por los platos más pedidos
+    dishesArray.sort((a, b) => b.quantity - a.quantity);
+    // Nos quedamos con los 5 platos más pedidos
+    dishesArray.length = 5;
+    // Convertimos el array en un objeto con las propiedades labels y data
+    const popularDishes = {
+      labels: dishesArray.filter((dish) => dish.createdAt.getFullYear() === year).map((dish) => dish.dishName),
+      data: dishesArray.filter((dish) => dish.createdAt.getFullYear() === year).map((dish) => dish.quantity),
+    };
 
     await client.query('COMMIT');
 
